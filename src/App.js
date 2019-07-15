942e031f700dc1ab199b09395611704a14d94300
petedz <ptdz@pm.me>
2019-07-15 08:42:14 -0400

    update

--- src/App.js
+++ src/App.js
@@ -1,25 +1,30 @@
 import React, { Component } from 'react';
-import logo from './logo.svg';
-import './App.css';
+import './static/App.css';
 
 class App extends Component {
   render() {
     return (
       <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <p>
-            Edit <code>src/App.js</code> and save to reload.
-          </p>
-          <a
-            className="App-link"
-            href="https://reactjs.org"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Learn React mother trucker!!
-          </a>
-        </header>
+        <Menu>
+          <Container>
+            <Menu.Item as="a" header>
+              <Image
+                  size="small"
+                  src="https://www.robinwieruch.de/img/page/logo.svg"
+              />
+            </Menu.Item>
+
+            <Menu.Menu position="right">
+              <Menu.Item as="a" name="login">
+                Login
+              </Menu.Item>
+
+              <Menu.Item as="a" name="register">
+                Register
+              </Menu.Item>
+            </Menu.Menu>
+          </Container>
+        </Menu>
       </div>
     );
   }
---
+++ src/components/menu.js
@@ -0,0 +1,27 @@
+import React, {Component} from 'react';
+import ReactDOM from 'react-dom';
+import App from "../index";
+
+
+class Menu extends Component{
+    render() {
+        const {menuItems} = this.props;
+        Return (
+            <li>
+                <a>
+
+                </a>
+            </li>
+        )
+
+    }
+}
+
+class MenuList extends React.Component{
+    render() {
+
+    }
+}
+
+
+ReactDOM.render(<App />, document.getElementById('root'));
